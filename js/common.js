726d5301d2b72c9d148287d19f8900f53ff35349
장평우 <33278815+pangnima@users.noreply.github.com>
2021-01-04 08:12:41 +0900

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,62 @@
+$(function () {
+
+	// 헤더 상단 붙임
+	$(window).on('scroll', headerFixed)
+
+	function headerFixed() {
+		var headerEl = $("header");
+		var headerH = headerEl.height();
+		var windowT = $(window).scrollTop();
+
+		if (windowT >= headerH) {
+			headerEl.addClass('active');
+		} else {
+			headerEl.removeClass('active');
+		}
+	}
+
+	// vnf
+	$('#fullpage').fullpage({
+		css3: false,
+		sectionsColor: ['', '#fff', '#666', '', '#666'],
+		navigation: true,
+		navigationPosition: 'right',
+		sectionSelector: '.section',
+		// scrollOverflow: true,
+		afterLoad: function (anchorLink, index) {
+			if (index == 5) {
+				$(".bd-left").stop(true).delay(50).animate({
+					"left": "0px",
+					"opacity": "1"
+				}, 500);
+				$(".bd-right").stop(true).delay(50).animate({
+					"left": "0px",
+					"opacity": "1"
+				}, 500);
+			} else {
+				$(".bd-left").stop(true).animate({
+					"left": "-300px",
+					"opacity": "0"
+				});
+				$(".bd-right").stop(true).animate({
+					"left": "300px",
+					"opacity": "0"
+				});
+			}
+
+			if ($(window).width() <= 767) {
+				if (index == 6) {
+					$(".banner").hide();
+				} else {
+					$(".banner").show();
+				}
+			}
+		}
+	});
+
+	if ($(window).width() >= 767) {
+		$(".special-box").hover(function () {
+			$(this).find(".hidden-txt").stop(true).slideToggle("fast");
+		});
+	}
+})
